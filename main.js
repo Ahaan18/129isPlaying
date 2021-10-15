cd3b11600ce44914c88a6636add1282ce0faf78a
Ahaan18 <71545644+Ahaan18@users.noreply.github.com>
2021-10-15 10:35:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+song1 = "";
+song2 = "";
+leftwristx = 0;
+rightwristx = 0;
+leftwristy = 0;
+rightwristy = 0;
+scoreleftwrist = 0;
+song1status = "";
+function preload(){
+    song1 = loadSound("destiny.mp3");
+    song2 = loadSound('live-while-were-young.mp3');
+}
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    x = ml5.poseNet(video, modeloaded);
+    x.on('pose', gotposes);
+}
+function draw(){
+    image(video, 0, 0, 600, 500);
+    stroke("#00FF00");
+    fill("#FF0000");
+}
+function modeloaded(){
+    console.log("AI DJ MUSIC APP™®© IS READY TO FUNCTION!");
+}
+function gotposes(result){
+    if(result.length > 0){
+        console.log(result);
+        rightwristy = result[0].pose.rightWrist.y;
+        leftwristy = result[0].pose.leftWrist.y;
+        leftwristx = result[0].pose.leftWrist.x;
+        rightwristx = result[0].pose.rightWrist.x;
+       scoreleftwrist = result[0].pose.keypoints[9].score;
+        console.log("rightwristx ", rightwristx, "y ", rightwristy, "leftwristx ", leftwristx, "y", leftwristy);
+    }
+}
