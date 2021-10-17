3b264ee01558939ad6db7874c535c7dcc3b8ac7b
Ahaan18 <71545644+Ahaan18@users.noreply.github.com>
2021-10-17 14:30:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,58 @@
 song1 = "";
 song2 = "";
-leftwristx = 0;
-rightwristx = 0;
-leftwristy = 0;
-rightwristy = 0;
-scoreleftwrist = 0;
+leftWristX = 0;
+rightWristX = 0;
+leftWristY = 0;
+rightWristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
 song1status = "";
+song2status = "";
 function preload(){
     song1 = loadSound("destiny.mp3");
     song2 = loadSound('live-while-were-young.mp3');
 }
 function setup(){
     canvas = createCanvas(600, 500);
-    canvas.center();
+    canvas.position(650, 220);
     video = createCapture(VIDEO);
     video.hide();
     x = ml5.poseNet(video, modeloaded);
     x.on('pose', gotposes);
 }
-function draw(){
-    image(video, 0, 0, 600, 500);
-    stroke("#00FF00");
-    fill("#FF0000");
-}
+
 function modeloaded(){
     console.log("AI DJ MUSIC APP™®© IS READY TO FUNCTION!");
 }
 function gotposes(result){
     if(result.length > 0){
         console.log(result);
-        rightwristy = result[0].pose.rightWrist.y;
-        leftwristy = result[0].pose.leftWrist.y;
-        leftwristx = result[0].pose.leftWrist.x;
-        rightwristx = result[0].pose.rightWrist.x;
-       scoreleftwrist = result[0].pose.keypoints[9].score;
-        console.log("rightwristx ", rightwristx, "y ", rightwristy, "leftwristx ", leftwristx, "y", leftwristy);
+        rightWristY = result[0].pose.rightWrist.y;
+        leftWristY = result[0].pose.leftWrist.y;
+        leftWristX = result[0].pose.leftWrist.x;
+        rightWristX = result[0].pose.rightWrist.x;
+       scoreLeftWrist = result[0].pose.keypoints[9].score;
+       scoreRightWrist = result[0].pose.keypoints[10].score;
+        console.log("rightwristx ", rightWristX, "y ", rightWristY, "leftwristx ", leftWristX, "y", leftWristY);
     }
+}
+function draw(){
+    image(video, 0, 0, 600, 500);
+    stroke("#00FF00");
+    fill("#FF0000");
+    song1status = song1.isPlaying();
+    song2status = song2.isPlaying();
+    if(scoreRightWrist > 0.2){
+        circle(rightWristX, rightWristY, 20);
+    }
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristX, leftWristY, 20);
+        song2.stop();
+        if(song1status = "false"){
+            song1.stop();
+song1.play();
+document.getElementById("naM").innerHTML = "Song name: Destiny";
+    }
+    }
+    
 }
